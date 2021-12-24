df692d0aac84af62510cc9e31ad6c55ba64a8a71
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2021-12-24 19:15:21 +0100

    blade

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Laravel</title>
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+            html {
+                line-height: 1.15;
+                -webkit-text-size-adjust: 100%;
+            }
+            body {
+                margin: 0;
+            }
+            a {
+                background-color: transparent;
+            }
+            [hidden] {
+                display: none;
+            }
+            html {
+                font-family: system-ui, -apple-system, BlinkMacSystemFont,
+                    Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans,
+                    sans-serif, Apple Color Emoji, Segoe UI Emoji,
+                    Segoe UI Symbol, Noto Color Emoji;
+                line-height: 1.5;
+            }
+            *,
+            :after,
+            :before {
+                box-sizing: border-box;
+                border: 0 solid #e2e8f0;
+            }
+            a {
+                color: inherit;
+                text-decoration: inherit;
+            }
+        </style>
+
+        <style>
+            body {
+                font-family: "Nunito", sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        @yield('content')
+    </body>
+</html>
--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    welcome to me
-</body>
-</html>
+@extends('layout.app') 
+@section('content')
+<div
+    class="
+        relative
+        flex
+        items-top
+        min-h-screen
+        bg-gray-100
+        dark:bg-gray-900
+        py-4
+        sm:pt-0
+    "
+>
+<a href="/">home</a>
+<a href="about">about</a>
+</div>
+@endsection
