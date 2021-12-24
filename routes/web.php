623c68e39c21d92317f77829182b029b9304ff1e
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2021-12-24 14:42:47 +0100

    serve static view

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('new', [
-    'uses' => 'pagesController@new'
-]);
+Route::get('new', function () {
+    return view('new');
+});
