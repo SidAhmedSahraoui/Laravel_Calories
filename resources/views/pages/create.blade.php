c4d356bbb88a448369b26a4440df3d1100096a4f
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2021-12-29 12:41:03 +0100

    fix form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -3,14 +3,12 @@
     <h1>
         Create New Post
     </h1>  
-              <form action="PostController@store" method="POST">
-                  <input type="text"  placeholder="title" >
-                  <textarea name="core" id="core" cols="30" rows="10" placeholder="Core" class="form-control">
-
-                  </textarea>
-                  <input type="number" name="calories" id="cal">
-                  <input type="submit" class="btn btn-primary">
-              </form>
+        {!! Form::open(['action' => 'App\Http\Controllers\PostController@store', 'method' => 'POST']) !!}
+                <div class="from-group">
+                    {{Form::label('title','Title')}}
+                    {{Form::text('title','', ['class'=>'form-control', 'placeholder'=>'Title'] )}}
+                </div>
+        {!! Form::close() !!}
     <a href="/posts"><button class="btn btn-secondary">Go Back</button></a>
 
        
